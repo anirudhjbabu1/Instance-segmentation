b45743cb0a12d6ed59e324a831fc759ec23c9ace
Anirudh J Babu <127778661+anirudhjbabu1@users.noreply.github.com>
2026-02-13 16:09:05 +0530

    Create Pretrained.py

---
+++ Pretrained.py
@@ -0,0 +1,23 @@
+# Pretrained Model
+
+import torchvision
+from torchvision.models.detection.mask_rcnn import MaskRCNN
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+# Load a pre-trained model (backbone) or define from scratch
+def get_model_instance_segmentation(num_classes):
+    # Load model with pre-trained backbone
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+   
+    # Replace the classifier head
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+   
+    # Replace the mask predictor head
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                       hidden_layer,
+                                                       num_classes)
+    return model
